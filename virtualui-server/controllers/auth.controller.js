e3de196974a4c04c39ec3bd1a7f5eecb1b40da29
charuljain02 <charulj706@gmail.com>
2026-08-16 10:49:32 +0530

    Update auth.controller.js

--- virtualui-server/controllers/auth.controller.js
+++ virtualui-server/controllers/auth.controller.js
@@ -23,8 +23,8 @@ export const googleAuth = async (req, res) => {
         const token = await genToken(user._id);
 
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
+            httpOnly: false,
+            secure: true,
             sameSite: "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
@@ -44,9 +44,9 @@ export const googleAuth = async (req, res) => {
 export const logOut = async (req, res) => {
     try {
         res.clearCookie("token", {
-            httpOnly: true,
-            secure: false,
-            sameSite: "strict"
+            httpOnly: false,
+            secure: true ,
+            sameSite: "none"
         });
 
         return res.status(200).json({
