e1ea8f38de629f839c1817b639203abb53cdcf0f
charuljain02 <charulj706@gmail.com>
2026-08-16 11:08:09 +0530

    Update index.js

--- virtualui-server/index.js
+++ virtualui-server/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://bitsnipai-4.onrender.com",
         credentials: true
     })
 );
